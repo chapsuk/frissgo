68312466baec34ee3da807a722ac3e5ae576e018
mkrasilnikov <m.krasilnikov@propellerads.net>
2017-02-04 17:14:52 +0300

    Created golang/go repo config

--- output/json.go
+++ output/json.go
@@ -5,5 +5,5 @@ import "github.com/chapsuk/gofriss/judge"
 type JSONFormatter struct{}
 
 func (jf *JSONFormatter) Format(t *judge.Top) []byte {
-	return nil
+	return []byte("text")
 }
--- output/text.go
+++ output/text.go
@@ -6,5 +6,5 @@ type TextFormatter struct {
 }
 
 func (tf *TextFormatter) Format(t *judge.Top) []byte {
-	return nil
+	return []byte("text")
 }
